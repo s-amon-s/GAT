c05a8f139aceb7c74385ddfe3f5c317096f3f616
Amon Misrha <muqtado3g@gmail.com>
2016-10-17 18:27:37 +0700

    Working on search filter complete

--- Geeta-Ashram/www/js/filters.js
+++ Geeta-Ashram/www/js/filters.js
@@ -2,13 +2,10 @@ angular.module('starter.filters', [])
 
 .filter('highlight', ['$http','$q','$rootScope', function($http,$q,$rootScope) {
     return function(text) {
-      console.log(text);
       var selectedWord = $('#seachKeyWord').val();
-      console.log('inside filter: '+ selectedWord);
       if(selectedWord!='') {
-      	console.log('inside Filter'+text)
         var pattern = new RegExp(selectedWord, "gi");
-        return text.replace(pattern, '[[' + selectedWord + ']]');
+        return text.replace(pattern, '<span class="highlighted">' + selectedWord + '</span>');
       }
       else {
         return text;
